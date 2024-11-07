cc692ecd99fa147bdf392711309f797ab24d7812
Madhur Jaripatke <madhur03mj@gmail.com>
2024-11-07 01:11:04 +0000

    Update EmployeeData.sol

--- LP-III/BCTL/EmployeeData.sol
+++ LP-III/BCTL/EmployeeData.sol
@@ -12,15 +12,16 @@ contract EmployeeDetails {
     }
 
     // Declare a variable to store the employee details
-    Employee public employee;
+    Employee[] public employee;
 
     // Function to create or update employee details
     function createEmployee(uint _id, string memory _name, uint _salary, string memory _joiningDate) public {
-        employee = Employee(_id, _name, _salary, _joiningDate);
+        employee.push(Employee(_id, _name, _salary, _joiningDate));
     }
 
     // Function to get employee details
-    function getEmployeeDetails() public view returns (uint, string memory, uint, string memory) {
-        return (employee.id, employee.name, employee.salary, employee.joiningDate);
-    }
+    function getEmployeeDetails(uint256 index) public view returns (uint, string memory, uint, string memory) {
+        require(index < employee.length, "Employee not found");
+        return (employee[index].id, employee[index].name, employee[index].salary, employee[index].joiningDate);
+    }
 }
